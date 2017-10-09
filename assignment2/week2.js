c7de4d2380fee6317bc816286ce1ca55927c2540
Alonsoag86 <alcoa729@newschool.edu>
2017-10-09 05:19:48 +0000

    fifth/sixth assignment

--- assignment2/week2.js
+++ assignment2/week2.js
@@ -6,37 +6,55 @@ var content = fs.readFileSync('/home/ubuntu/workspace/assignment1/data/m09.txt')
 
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
-var finalAddress = []
 
-/*
-$ ('td').each(function(i, element){
-   if ($(element.style == 'border-bottom: 1px solid #e3e3e3; width: 260px')) {
-       
-      var address = $(this).contents();
-      console.log (address.text());
-   }
-});
 
-//too much info as a result
-*/
+var finalAddress = [];
+var wheelchair = [];
+var meetingName = [];
+var locationName = [];
 
-/*
+
+
  $('div.detailsBox').each(function(i, element){
-      var firstStep = $(this) .prev();
-      var address = firstStep .prev() .prev() .prev() .prev() .prev() .prev();
-      console.log(address.text().split("<br>"));
+      var location = $(this) .prev() .prev() .prev() .prev() .prev() .prev() .prev() .text();
+      locationName.push(location);
     });
-    
-//you just get the places names (not the third one tho)    
-*/
+//Code for the location name (not the third one tho)    
+
+
 
 $('div.detailsBox').each(function(i, element){
       var address = $(element) .parent() .contents() .slice(3) .eq(3) .text() .trim();
       finalAddress.push(address);
       });
-      
-console.log(finalAddress);
     
 //"On the Internet, nobody knows you're a dog"
 //Really? Well, it seems pretty obvious that a 
 //dog wrote the code for the AA meetings web page
+
+//Code for the address
+
+
+$('div.detailsBox').each(function(i, element){
+      var meetingName1 = $(element) .parent() .contents() .slice(2) .eq(2) .text() .trim();
+      meetingName.push(meetingName1);
+      console.log(meetingName);
+      });
+//This code gets you the meeting name
+
+
+
+
+
+$ ('td').each(function(i, element){
+   if ($(element.style == "border-bottom:1px solid #e3e3e3;width:350px;")) {
+       
+      var wheelchair1 = $(this) .contents() .nextAll() .slice(4) .eq(4);
+      wheelchair.push(wheelchair1.text() .trim());
+   }
+});
+
+console.log(finalAddress);
+console.log(locationName);
+console.log (wheelchair);
+console.log(meetingName);
---
+++ assignment5-6/clean.js
@@ -0,0 +1,90 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+// load the thesis text file into a variable, `content`
+var content = fs.readFileSync('/home/ubuntu/workspace/assignment1/data/m09.txt');
+
+// load `content` into a cheerio object
+var $ = cheerio.load(content);
+
+var finalOutput = [];
+
+$('table table table tbody').find('tr').each(function(i, elem){
+    
+var missingInfo = $(elem).find('td').eq(1).html().split('<br>');
+  for (var i = 0; i < missingInfo.length; i ++){
+      if (missingInfo[i].match(/ From/g) !== null) {
+        
+        var idealObject = {};
+        
+        $('div.detailsBox').each(function(i, element){
+        var address1 = $(element) .parent() .contents() .slice(3) .eq(3) .text() .trim();
+        var address2 = address1.substring(0, address1.indexOf(','));
+        var address3 = address2 + ', New York, NY';
+        idealObject.googleAddress = address3.split(' ').join('+');
+        });
+        
+         $('div.detailsBox').each(function(i, element){
+        var address1 = $(element) .parent() .contents() .slice(3) .eq(3) .text() .trim();
+        var address2 = address1.substring(0, address1.indexOf(','));
+        var address3 = address2 + ', New York, NY';
+        idealObject.address = address3.toString();
+        });
+        
+        var daysOfWeek = missingInfo[i].match(/Mondays|Tuesdays|Wednesdays|Thursdays|Fridays|Saturdays|Sundays/gi);
+        idealObject.weekDays = daysOfWeek.toString();
+        
+        var startTimes = missingInfo[i].match(/\d.+/gi);
+        idealObject.startTime = startTimes.toString().split(' <b>')[0];
+        
+        var endTimes = missingInfo[i].match(/\d.+/gi);
+        idealObject.endTime = endTimes.toString().split('</b> ')[1].slice(0, -1);
+
+        $(elem).find('div.detailsBox').each(function(i, element){
+        idealObject.location = $(this) .prev() .prev() .prev() .prev() .prev() .prev() .prev() .text();
+        
+        $(elem).find('div.detailsBox').each(function(i, element){
+        idealObject.meetingName = $(element) .parent() .contents() .slice(2) .eq(2) .text() .trim();
+        
+        $('table table table tbody').find('tr').each(function(i, elem){
+        
+        idealObject.meeting_Type = $(elem).find('td').eq(1).contents().filter(function(){ 
+        return this.nodeType == 3;
+        }).eq(3).text().trim();
+        });
+        
+        $(elem).find('td').eq(0).each(function(i, element){
+        idealObject.wheelchair = $(this) .contents() .nextAll() .slice(4) .eq(4).text() .trim();
+        
+        $(elem).find('div.detailsBox').each(function(i, element){
+        idealObject.details = $(element) .contents() .text() .trim();
+        
+        
+        finalOutput.push(idealObject);
+        
+            
+        });
+   
+        });
+        
+        });
+    
+            
+    });
+
+        //console.log(idealObject);
+      }
+  }
+  
+});
+
+require('fs').writeFile('nolatlong.json',
+    JSON.stringify(finalOutput),
+    function (err) {
+        if (err) {
+            console.error('error');
+        }
+    }
+);
+
+console.log(finalOutput);
