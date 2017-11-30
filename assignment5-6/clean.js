b9aee9dbecbc4cd146f78c0a38de6e0b8c001be4
Alonsoag86 <alcoa729@newschool.edu>
2017-11-30 20:30:08 +0000

    fifth and sixth assignments

--- assignment5-6/clean.js
+++ assignment5-6/clean.js
@@ -24,8 +24,19 @@ function populateArray () {
         
         var firstCell = $(row).find('td').eq(0); // this is where you'll find all the information for the events. You don't need a for loop here since there's no point on repeating this information twice.
         
-        thisMeeting.eventName = firstCell.find('b').text(); // get event name
-        
+        var name = firstCell.find('b').text(); // get event name
+        var firstName = name.substr(0, name.indexOf('-') - 1); // get first part of the event name
+        var secondName = name.substr(name.indexOf('-') + 2); // get second part of the event name
+        if (firstName == secondName.toUpperCase()) {
+        thisMeeting.eventName = firstName;
+        } else if (secondName == "") {
+        thisMeeting.eventName = firstName;    
+        } else if (firstName != secondName) {
+        thisMeeting.eventName = firstName + " " + secondName.split(' - ')[0] } else {
+        thisMeeting.eventName = secondName.toUpperCase();    
+        }
+        //console.log(thisMeeting.eventName);
+
         if (firstCell.find('h4').text() != "") {              // if there's no empty space
          thisMeeting.location = firstCell.find('h4').text();  // get location
         }                                                     // name
@@ -41,33 +52,70 @@ function populateArray () {
         var meetingsSplit = secondCell.html().split(' <br/> <br/> <b>'); //this code will make it easier to read the contents and separate them later.
         // iterating over meeting times, days and information on the second cell.
         for (var t = 0; t < meetingsSplit.length; t++) {
-            
+        //console.log(meetingsSplit);    
             var thisDay = new Object(); // doing this will create a new object for every separate day as you iterate over the contents of the second cell.
-            thisDay.day = meetingsSplit[t].replace('s', 'Sundays').match(/Mondays|Tuesdays|Wednesdays|Thursdays|Fridays|Saturdays|Sundays/gi).toString();
+            thisDay.day = meetingsSplit[t].match(/\w.+/gi).toString().split(' <b>')[0].split(' From')[0].replace('b>', '');
+            if (thisDay.day == 's') {
+            thisDay.day = 'Sundays';    
+            }
+            //console.log(thisDay.day);
+            //thisDay.day = meetingsSplit[t].match(/Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/gi).toString();
+            //console.log(thisDay.day);
+            //thisDay.dayNumber = meetingsSplit[t].match(/Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/gi).toString();
+            if (thisDay.day === 'Sundays') {
+            thisDay.dayNumber = 0;
+            } else if (thisDay.day === 'Mondays') {
+            thisDay.dayNumber = 1;
+            } else if (thisDay.day === 'Tuesdays') {
+            thisDay.dayNumber = 2;
+            } else if (thisDay.day === 'Wednesdays') {
+            thisDay.dayNumber = 3;    
+            } else if (thisDay.day === 'Thursdays') {
+            thisDay.dayNumber = 4;    
+            } else if (thisDay.day === 'Fridays') {
+            thisDay.dayNumber = 5;    
+            } else if (thisDay.day === 'Saturdays') {
+            thisDay.dayNumber = 6;
+            }
             var firstTime = meetingsSplit[t].match(/\d.+/gi).toString().split(' <b>')[0];
             var separate = firstTime.split(' ');
             var timeOfDay = separate[1];
+            if (timeOfDay == 'P') {
+            timeOfDay = 'PM';    
+            }
             var stepOne = separate[0];
             var stepTwo = stepOne.split(':');
             var hour = +stepTwo[0];
             var minute = +stepTwo[1];
-            if (timeOfDay === 'PM' && hour < 12) {
-            thisDay.startTime = hour + 12 + ':' + minute;
-            } else {
-            thisDay.startTime = hour + ':' + minute;
+            if (timeOfDay === 'AM' && hour === 12) {
+            thisDay.startTime = 00 + '.' + minute;
             }
+            if (timeOfDay === 'PM' && hour < 12) {
+            thisDay.startTime = hour + 12 + '.' + minute;
+            } else {
+            thisDay.startTime = hour + '.' + minute;
+            }
+            thisDay.startTime = +thisDay.startTime * 1;
             var secondTime = meetingsSplit[t].match(/\d.+/gi).toString().split('</b> ')[1].slice(0, 7);
             var separateTwo = secondTime.split(' ');
             var timeOfDayTwo = separateTwo[1];
+            if (timeOfDayTwo == 'P') {
+            timeOfDayTwo = 'PM';    
+            }
             var stepOneEndTime = separateTwo[0];
             var stepTwoEndTime = stepOneEndTime.split(':');
             var hourTwo = +stepTwoEndTime[0];
             var minuteTwo = +stepTwoEndTime[1];
-            if (timeOfDayTwo === 'PM' && hourTwo < 12) {
-            thisDay.endTime = hourTwo + 12 + ':' + minuteTwo;
-            } else {
-            thisDay.endTime = hourTwo + ':' + minuteTwo;
+            if (timeOfDayTwo === 'AM' && hourTwo === 12) {
+            thisDay.endTime = 00 + '.' + minuteTwo;
             }
+            if (timeOfDayTwo === 'PM' && hourTwo < 12) {
+            thisDay.endTime = hourTwo + 12 + '.' + minuteTwo;
+            } else {
+            thisDay.endTime = hourTwo + '.' + minuteTwo;
+            }
+            thisDay.endTime = +thisDay.endTime * 1;
+            //console.log(thisDay.endTime);
             thisDay.from = firstTime;
             thisDay.to = secondTime;
             //thisDay.startTime = meetingsSplit[t].match(/\d.+/gi).toString().split(' <b>')[0]; // get start time.
@@ -90,7 +138,7 @@ function populateArray () {
         thisAddress.street = address3.toString();
         
         thisMeeting.address = thisAddress;
-    
+        
         
         finalOutput.push(thisMeeting); // push everything to your outer most array.
         
